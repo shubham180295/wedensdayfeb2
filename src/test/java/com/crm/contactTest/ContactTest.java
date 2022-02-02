a070112403cee9509d51f87fa06d5c29a5b9a6d1
shubham180295@gmail.com <Shubham180295@gmail.com>
2022-02-02 15:35:13 +0530

    change the password

--- src/test/java/com/crm/contactTest/ContactTest.java
+++ src/test/java/com/crm/contactTest/ContactTest.java
@@ -6,16 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ContactTest {
-	static {
-		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
-	}
 	
 	@Test(groups = "smokeTest")
 	public void createContactTest() {
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://www.facebook.com/");
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("shubham180295@gmail.com");
-		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("sanu180295@");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("9977141550@");
+		//driver.findElement(By.xpath("//input[@type='password']")).sendKeys("sanu180295@");
 		driver.findElement(By.xpath("//button[@name='login']")).submit();
 		System.out.println("execute createContactTest");
 		System.out.println("login");
